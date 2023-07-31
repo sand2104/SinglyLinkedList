1d1ee1440c43b9a113097828e3f34f5c204b5aa1
sand2104 <prasadsandy2000@gmail.com>
2023-07-31 23:30:12 +0530

    first commit

---
+++ Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phase1Section4._7b
+{
+    public class Node
+    {
+        private string data;
+        private Node next = null;
+        public string Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+        public Node Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
+    }
+}
